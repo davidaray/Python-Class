486f72496e870b400fcb109c765173a32703f674
Kevin Sullivan <kev.am.sullivan@gmail.com>
2019-11-13 12:00:31 -0600

    Add files via upload

--- doLift.py
+++ doLift.py
@@ -3,15 +3,11 @@ from pathlib import Path
 import os
 
 
-def buildDoLift
-	#x = unknown.2bit
-	#twoBitPrefix = re.search('\w+', x)
-	y = re.search('\w+', twoBit)
+def buildDoLift(GENOME)
+	y = re.search('\w+', GENOME)
 	twoBitPrefix = y[0]
 	print("Creating doLift.sh file...\n")
 	OUT = open('doLift.sh', 'w+')
-	#commenting out the lines below because I think I can just include them in the example script
-	##	OUT.write("#\! /bin/csh\n")
 	OUT.write( "#\$ -V\n")
 	OUT.write( "#\$ -cwd\n")
 	OUT.write( "#\$ -N $twoBit-doLift\n")
@@ -20,83 +16,44 @@ def buildDoLift
 	OUT.write( "#\$ -q omni\n")
 	OUT.write( "#\$ -pe sm 1\n")
 	OUT.write( "#\$ -P quanah\n")
-	print OUT <<_EOF_ #???????????
 	partitionDir=Path(os.getcwd())
 	OUT.write(partitionDir"/..\n")
-	#cd partitionDir/..
-	#os.chdir(partitionDir.parent)
-	#os.chdir("..") do we need to specify partitionDir?
-
-	with open("doLift-template.sh") as n:
-		for line in n:
-			OUT.write(line)
-
-
-#also no idea what "1;" does at the end of hte file 
---------- #OR		
-##	OUT.write("#\! /bin/csh\n")
-##	OUT.write( "#\$ -V\n")
-##	OUT.write( "#\$ -cwd\n")
-##	OUT.write( "#\$ -N $twoBit-doLift\n")
-##	OUT.write( "#\$ -o \$JOB_NAME.o.\$JOB_ID\n")
-##	OUT.write( "#\$ -e \$JOB_NAME.e.\$JOB_ID\n")
-##	OUT.write( "#\$ -q omni\n")
-##	OUT.write( "#\$ -pe sm 1\n")
-##	OUT.write( "#\$ -P quanah\n")
-			
-##	OUT.write('foreach d0 ( RMPart/??? )')
-##	OUT.write('  set bNum = \$d0:t')
-##	OUT.write('  $ucscBinDir/liftUp -type=.out stdout \$d0/\$bNum.lft error \$d0/\$bNum.fa.out > \$d0/\$bNum.fa.liftedOut')
-##	OUT.write('  if ( -e \$d0/\$bNum.fa.align ) then')
-##	OUT.write('    $ucscBinDir/liftUp -type=.align stdout \$d0/\$bNum.lft error \$d0/\$bNum.fa.align > \$d0/\$bNum.fa.liftedAlign')
-##	OUT.write('  endif')
-##	OUT.write('end')
-##
-##	OUT.write('$ucscBinDir/liftUp $twoBitPrefix.fa.out /dev/null carry RMPart/???/*.liftedOut')
-##	OUT.write('$ucscBinDir/liftUp $twoBitPrefix.fa.align /dev/null carry RMPart/???/*.liftedAlign')
-##
-##	OUT.write(' ##Create a summary file')
-##	OUT.write('# In some cases the file system delays cause the *.out file not to be available ')
-##	OUT.write('# Give it some time for things to settle down')
-##	OUT.write('sleep 30')
-##	OUT.write('$repeatMasker/util/buildSummary.pl -useAbsoluteGenomeSize -genome $twoBit $twoBitPrefix.fa.out > $twoBitPrefix.summary')
-##	OUT.write('gzip -f $twoBitPrefix.summary')
-##
-##	OUT.write(' ##Generate RepeatLandscape')
-##	OUT.write('perl $repeatMasker/util/calcDivergenceFromAlign.pl -s $twoBitPrefix.divsum $twoBitPrefix.fa.align')
-##	OUT.write('perl $repeatMasker/util/createRepeatLandscape.pl -div $twoBitPrefix.divsum -twoBit $twoBit > $twoBitPrefix-landscape.html')
-##	OUT.write('gzip -f $twoBitPrefix.divsum')
-##	OUT.write('gzip -f $twoBitPrefix-landscape.html')
-##
-##
-##
-##	OUT.write(' ##NOTE NOTE NOTE: Only useful for UCSC')
-##	OUT.write(' ##Generate data for the UCSC browser tracks')
-##
-##	OUT.write('if ( -e $twoBitPrefix.fa.out && -e $twoBitPrefix.fa.align ) then')
-##	OUT.write('  $repeatMasker/util/rmToUCSCTables.pl -out $twoBitPrefix.fa.out -align $twoBitPrefix.fa.align')
-##	OUT.write('  gzip -f $twoBitPrefix.fa.out.tsv')
-##	OUT.write('  gzip -f $twoBitPrefix.fa.align.tsv')
-##	OUT.write('  gzip -f $twoBitPrefix.fa.join.tsv')
-##	OUT.write('endif ')
-##
-##
-##	OUT.write(' ##NOTE NOTE NOTE: Only useful to ISB ')
-##	OUT.write(' ##Generate files for loading into RMDB')
-##
-##	OUT.write('cat $twoBitPrefix.fa.out | /home/rhubley/cAlign > $twoBitPrefix.fa.out.c')
-##	OUT.write('cat $twoBitPrefix.fa.align | /home/rhubley/cAlign > $twoBitPrefix.fa.align.c')
-##	OUT.write('gzip -f $twoBitPrefix.fa.out.c')
-##	OUT.write('gzip -f $twoBitPrefix.fa.align.c')
-##
-##	OUT.write(' ##Zip up final results')
-##	OUT.write('gzip -f $twoBitPrefix.fa.out')
-##	OUT.write('gzip -f $twoBitPrefix.fa.align')
-##
-##	OUT.write('_EOF_')
-##
-##	OUT.write('  close OUT;')
-##1;
-##
-
-
+	OUT.write('foreach d0 ( RMPart/??? )\n')
+	OUT.write('  set bNum = \$d0:t\n')
+	OUT.write('  $ucscBinDir/liftUp -type=.out stdout \$d0/\$bNum.lft error \$d0/\$bNum.fa.out > \$d0/\$bNum.fa.liftedOut\n')
+	OUT.write('  if ( -e \$d0/\$bNum.fa.align ) then\n')
+	OUT.write('    $ucscBinDir/liftUp -type=.align stdout \$d0/\$bNum.lft error \$d0/\$bNum.fa.align > \$d0/\$bNum.fa.liftedAlign\n')
+	OUT.write('  endif\n')
+	OUT.write('end\n')
+	OUT.write('$ucscBinDir/liftUp $twoBitPrefix.fa.out /dev/null carry RMPart/???/*.liftedOut\n')
+	OUT.write('$ucscBinDir/liftUp $twoBitPrefix.fa.align /dev/null carry RMPart/???/*.liftedAlign\n')
+	OUT.write(' Create a summary file\n')
+	OUT.write('# In some cases the file system delays cause the *.out file not to be available \n')
+	OUT.write('# Give it some time for things to settle down\n')
+	OUT.write('sleep 30\n')
+	OUT.write('$repeatMasker/util/buildSummary.pl -useAbsoluteGenomeSize -genome $twoBit $twoBitPrefix.fa.out > $twoBitPrefix.summary\n')
+	OUT.write('gzip -f $twoBitPrefix.summary\n')
+	OUT.write(' Generate RepeatLandscape\n')
+	OUT.write('perl $repeatMasker/util/calcDivergenceFromAlign.pl -s $twoBitPrefix.divsum $twoBitPrefix.fa.align\n')
+	OUT.write('perl $repeatMasker/util/createRepeatLandscape.pl -div $twoBitPrefix.divsum -twoBit $twoBit > $twoBitPrefix-landscape.html\n')
+	OUT.write('gzip -f $twoBitPrefix.divsum\n')
+	OUT.write('gzip -f $twoBitPrefix-landscape.html\n')
+	OUT.write(' NOTE NOTE NOTE: Only useful for UCSC\n')
+	OUT.write(' Generate data for the UCSC browser tracks\n')
+	OUT.write('if ( -e $twoBitPrefix.fa.out && -e $twoBitPrefix.fa.align ) then\n')
+	OUT.write('  $repeatMasker/util/rmToUCSCTables.pl -out $twoBitPrefix.fa.out -align $twoBitPrefix.fa.align\n')
+	OUT.write('  gzip -f $twoBitPrefix.fa.out.tsv\n')
+	OUT.write('  gzip -f $twoBitPrefix.fa.align.tsv\n')
+	OUT.write('  gzip -f $twoBitPrefix.fa.join.tsv\n')
+	OUT.write('endif \n')$
+	OUT.write(' NOTE NOTE NOTE: Only useful to ISB \n')
+	OUT.write(' Generate files for loading into RMDB\n')
+	OUT.write('cat $twoBitPrefix.fa.out | /home/rhubley/cAlign > $twoBitPrefix.fa.out.c\n')
+	OUT.write('cat $twoBitPrefix.fa.align | /home/rhubley/cAlign > $twoBitPrefix.fa.align.c\n')
+	OUT.write('gzip -f $twoBitPrefix.fa.out.c\n')
+	OUT.write('gzip -f $twoBitPrefix.fa.align.c\n')
+	OUT.write(' Zip up final results\n')
+	OUT.write('gzip -f $twoBitPrefix.fa.out\n')
+	OUT.write('gzip -f $twoBitPrefix.fa.align\n')
+	OUT.write('_EOF_\n')
+	OUT.write('  close OUT;\n')
